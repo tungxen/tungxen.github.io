b82c06027496431d1faccb294997b623a8afeb15
tungxen <tungtungxen@gmail.com>
2023-09-12 22:03:52 +0700

    Add files via upload

---
+++ manga_ocr/yoku-wakaranai/yoku-wakaranai-keredo-isekai-ni-tensei-shiteita-you-desu-chapter-29-2.js
@@ -0,0 +1,638 @@
+var ocr_data = {
+    "001.jpg": [
+        {
+            "x1": 490,
+            "y1": 116,
+            "x2": 553,
+            "y2": 286,
+            "text": "………どこかで 鍛治場を借りることって 出来ないでしょうか？",
+            "vi": "Bạn có thể thuê một sân đào tạo ở đâu đó không?"
+        },
+        {
+            "x1": 502,
+            "y1": 382,
+            "x2": 550,
+            "y2": 464,
+            "text": "鍛治場 ですか？",
+            "vi": "Nó có phải là một sân tập?"
+        },
+        {
+            "x1": 52,
+            "y1": 410,
+            "x2": 115,
+            "y2": 519,
+            "text": "はいちょっと 鍛治ッスキルを 鍛えたいので",
+            "vi": "Tôi muốn đào tạo các kỹ năng của mình một chút"
+        },
+        {
+            "x1": 549,
+            "y1": 608,
+            "x2": 567,
+            "y2": 642,
+            "text": "ええ？",
+            "vi": "Ừ?"
+        },
+        {
+            "x1": 470,
+            "y1": 607,
+            "x2": 526,
+            "y2": 747,
+            "text": "持ってるんですか？ というが剣を作ったり するんですか？",
+            "vi": "Bạn có nó không? Nhưng bạn có kiếm một thanh kiếm không?"
+        },
+        {
+            "x1": 221,
+            "y1": 605,
+            "x2": 278,
+            "y2": 758,
+            "text": "ええ一応 今使ってるのも 自分で作たものてす",
+            "vi": "Vâng, những gì tôi sử dụng bây giờ là những gì tôi đã làm"
+        },
+        {
+            "x1": 37,
+            "y1": 714,
+            "x2": 115,
+            "y2": 868,
+            "text": "それでちょっと 作りたいものがあるので 舐治スキルを 上げておきたしんです",
+            "vi": "Vì vậy, có một cái gì đó tôi muốn thực hiện, vì vậy tôi đã nâng cao kỹ năng liếm của mình."
+        }
+    ],
+    "002.jpg": [
+        {
+            "x1": 528,
+            "y1": 95,
+            "x2": 596,
+            "y2": 280,
+            "text": "…それだったら 私の伝手で紹介 出来るかも知れないよ",
+            "vi": "Nếu vậy, tôi có thể giới thiệu nó với tiểu sử của tôi."
+        },
+        {
+            "x1": 210,
+            "y1": 92,
+            "x2": 229,
+            "y2": 203,
+            "text": "アリサさん？",
+            "vi": "Alisa?"
+        },
+        {
+            "x1": 84,
+            "y1": 234,
+            "x2": 126,
+            "y2": 360,
+            "text": "あ そっか アリサの家って…",
+            "vi": "À, nhà của Alisa..."
+        },
+        {
+            "x1": 550,
+            "y1": 433,
+            "x2": 591,
+            "y2": 589,
+            "text": "私の家は代々 剣士の家来なんだよ",
+            "vi": "Nhà tôi là người hầu của một kiếm sĩ qua nhiều thế hệ"
+        },
+        {
+            "x1": 170,
+            "y1": 447,
+            "x2": 235,
+            "y2": 589,
+            "text": "それで すっと品にてる 鍛治工房があってー",
+            "vi": "Vì vậy, có một hội thảo quy trình là một sản phẩm tốt."
+        },
+        {
+            "x1": 85,
+            "y1": 513,
+            "x2": 163,
+            "y2": 609,
+            "text": "そこの親方に お願いすれば もしかすると なんだけどー",
+            "vi": "Nếu bạn hỏi Master ở đó, có lẽ"
+        },
+        {
+            "x1": 567,
+            "y1": 701,
+            "x2": 587,
+            "y2": 779,
+            "text": "なるほど",
+            "vi": "Tôi thấy"
+        },
+        {
+            "x1": 89,
+            "y1": 702,
+            "x2": 224,
+            "y2": 871,
+            "text": "それで どれくらいの間 借りたいのー？ それに借りるとなると お金も必要になると 思う大丈夫—？",
+            "vi": "Vậy bạn muốn mượn bao lâu? Bạn có nghĩ rằng bạn sẽ cần tiền nếu bạn vay nó?"
+        }
+    ],
+    "003.jpg": [
+        {
+            "x1": 512,
+            "y1": 106,
+            "x2": 553,
+            "y2": 259,
+            "text": "お金のほうは 多分足りると思います",
+            "vi": "Tôi nghĩ rằng tiền có lẽ là đủ"
+        },
+        {
+            "x1": 442,
+            "y1": 118,
+            "x2": 482,
+            "y2": 288,
+            "text": "足りなければ頑張って 薬草採取して稼きますし",
+            "vi": "Nếu bạn không có đủ, bạn sẽ cố gắng hết sức để thu thập các loại thảo dược thuốc"
+        },
+        {
+            "x1": 58,
+            "y1": 89,
+            "x2": 143,
+            "y2": 216,
+            "text": "それで借りる 期間の方ですけど 最低でも半年は 借りたいです",
+            "vi": "Vì vậy, đó là thời gian vay, nhưng tôi muốn mượn ít nhất sáu tháng"
+        },
+        {
+            "x1": 518,
+            "y1": 375,
+            "x2": 549,
+            "y2": 481,
+            "text": "半年も⁉",
+            "vi": "Sáu tháng"
+        },
+        {
+            "x1": 524,
+            "y1": 569,
+            "x2": 539,
+            "y2": 724,
+            "text": "目標だったりするがらね",
+            "vi": "Đó là một mục tiêu"
+        },
+        {
+            "x1": 452,
+            "y1": 645,
+            "x2": 512,
+            "y2": 800,
+            "text": "集中じて鍛えるなら 半年もあればいけそらな 気もする",
+            "vi": "Nếu bạn tập trung và đào tạo, bạn có thể cảm thấy như là sáu tháng"
+        },
+        {
+            "x1": 390,
+            "y1": 745,
+            "x2": 445,
+            "y2": 873,
+            "text": "もも半年でそこまて いけなかったらっ 頭を下げて",
+            "vi": "Nếu bạn không thể đến đó trong sáu tháng, hãy cúi đầu"
+        },
+        {
+            "x1": 54,
+            "y1": 714,
+            "x2": 117,
+            "y2": 858,
+            "text": "えっと お願いしても 大丈夫でしょうか？",
+            "vi": "Có thể hỏi không?"
+        }
+    ],
+    "004.jpg": [
+        {
+            "x1": 521,
+            "y1": 83,
+            "x2": 599,
+            "y2": 210,
+            "text": "まかせてー！ 美味しいもの 沢山食べさせて 貰ってるからねー",
+            "vi": "Để lại nó! Tôi cho tôi rất nhiều món ăn ngon"
+        },
+        {
+            "x1": 518,
+            "y1": 293,
+            "x2": 601,
+            "y2": 405,
+            "text": "それで レンさんは どんな剣が 作りたいのー？",
+            "vi": "Vậy bạn muốn thực hiện loại thanh kiếm nào?"
+        },
+        {
+            "x1": 85,
+            "y1": 279,
+            "x2": 148,
+            "y2": 420,
+            "text": "魔力剣？ 属性剣？ 錬金術とか言ってたし もしかして魔剣？",
+            "vi": "Kiếm ma thuật? Thuộc tính thanh kiếm? Bạn có nói thuật giả kim, có thể là một thanh kiếm ma thuật?"
+        },
+        {
+            "x1": 521,
+            "y1": 476,
+            "x2": 578,
+            "y2": 564,
+            "text": "魔力剣？ 属性剣？",
+            "vi": "Kiếm ma thuật? Thuộc tính thanh kiếm?"
+        },
+        {
+            "x1": 526,
+            "y1": 716,
+            "x2": 589,
+            "y2": 858,
+            "text": "あ じゃあ 説明しまーす",
+            "vi": "Oh, tôi sẽ giải thích nó"
+        }
+    ],
+    "005.jpg": [
+        {
+            "x1": 535,
+            "y1": 91,
+            "x2": 567,
+            "y2": 175,
+            "text": "魔力剣",
+            "vi": "Kiếm ma thuật"
+        },
+        {
+            "x1": 407,
+            "y1": 136,
+            "x2": 506,
+            "y2": 275,
+            "text": "無属性魔法が 付与された剣の事て 普通の剣よりも強力で なおかっ耐久性も 優れている",
+            "vi": "Thanh kiếm không có phép thuật không chính xác mạnh mẽ và bền hơn thanh kiếm thông thường."
+        },
+        {
+            "x1": 63,
+            "y1": 126,
+            "x2": 143,
+            "y2": 295,
+            "text": "度力が付与されているので 霊体やアンデッドにも青効 そして無偶性ゆえに 汎用性が高い",
+            "vi": "Vì mức độ bằng cấp, nó rất linh hoạt do hiệu ứng màu xanh trên tinh thần và xác sống."
+        },
+        {
+            "x1": 544,
+            "y1": 397,
+            "x2": 573,
+            "y2": 479,
+            "text": "属性剣",
+            "vi": "Thuộc tính Kiếm"
+        },
+        {
+            "x1": 450,
+            "y1": 426,
+            "x2": 491,
+            "y2": 552,
+            "text": "各種の屈性魔法が 付与された剣",
+            "vi": "Một thanh kiếm với nhiều loại ma thuật điên rồ"
+        },
+        {
+            "x1": 55,
+            "y1": 427,
+            "x2": 148,
+            "y2": 548,
+            "text": "それそれの屈性に 特化してるので 得手不得手が はっきりしている分 はまれは強力",
+            "vi": "Nó rất hiếm vì nó được chuyên môn hóa của nó, vì vậy rất hiếm khi rõ ràng là nó rõ ràng."
+        },
+        {
+            "x1": 519,
+            "y1": 662,
+            "x2": 553,
+            "y2": 799,
+            "text": "されたかな 魔力剣または魔性剣は",
+            "vi": "Thật là một thanh kiếm ma thuật hoặc một thanh kiếm ma thuật"
+        },
+        {
+            "x1": 59,
+            "y1": 713,
+            "x2": 113,
+            "y2": 880,
+            "text": "時間を置けは回短するけ 回復までに掛がる時間は まちまち",
+            "vi": "Thời gian ngắn và thời gian để phục hồi sẽ khác nhau"
+        }
+    ],
+    "006.jpg": [
+        {
+            "x1": 592,
+            "y1": 77,
+            "x2": 616,
+            "y2": 136,
+            "text": "そして",
+            "vi": "Và"
+        },
+        {
+            "x1": 553,
+            "y1": 129,
+            "x2": 587,
+            "y2": 191,
+            "text": "魔剣",
+            "vi": "Kiếm ma thuật"
+        },
+        {
+            "x1": 458,
+            "y1": 130,
+            "x2": 516,
+            "y2": 270,
+            "text": "届性魔法たけではなく スキル効果が 付与された剣",
+            "vi": "Một thanh kiếm có hiệu ứng kỹ năng, không phải là một loại nấm ma thuật thông báo"
+        },
+        {
+            "x1": 204,
+            "y1": 77,
+            "x2": 220,
+            "y2": 230,
+            "text": "付与されたスキル効界は",
+            "vi": "Thế giới kỹ năng đã cho là gì?"
+        },
+        {
+            "x1": 163,
+            "y1": 76,
+            "x2": 181,
+            "y2": 177,
+            "text": "耐久力強化”等",
+            "vi": "Tăng cường độ bền, vv"
+        },
+        {
+            "x1": 105,
+            "y1": 133,
+            "x2": 142,
+            "y2": 234,
+            "text": "珍いものだと 幸運なんて",
+            "vi": "May mắn thay, nếu nó bất thường"
+        },
+        {
+            "x1": 75,
+            "y1": 284,
+            "x2": 292,
+            "y2": 430,
+            "text": "なんとか ライ半",
+            "vi": "Bằng cách nào đó Rai nửa"
+        },
+        {
+            "x1": 519,
+            "y1": 383,
+            "x2": 618,
+            "y2": 536,
+            "text": "また 属性魔法が付らされすに 特殊なスキルのみが 付与されたものも 分類上は瞬剣になり",
+            "vi": "Ngoài ra, chỉ có các kỹ năng đặc biệt được cấp cho phép thuật thuộc tính, đó là một thanh kiếm tức thì trong phân loại."
+        },
+        {
+            "x1": 462,
+            "y1": 446,
+            "x2": 500,
+            "y2": 560,
+            "text": "更に物によっては 武器固有の特殊な",
+            "vi": "Hơn nữa, tùy thuộc vào sự việc, một vũ khí đặc biệt đặc biệt"
+        },
+        {
+            "x1": 96,
+            "y1": 417,
+            "x2": 152,
+            "y2": 581,
+            "text": "厨二心が くすぐられます",
+            "vi": "Nhà bếp hai trái tim cù lét"
+        },
+        {
+            "x1": 538,
+            "y1": 635,
+            "x2": 597,
+            "y2": 762,
+            "text": "度剣の作成には 基本的に錬金怖が 必要ららじい",
+            "vi": "Về cơ bản, thuật giả kim được yêu cầu để tạo ra một thanh kiếm"
+        },
+        {
+            "x1": 457,
+            "y1": 669,
+            "x2": 523,
+            "y2": 806,
+            "text": "また心ても 錬金術か",
+            "vi": "Có phải là thuật giả kim không?"
+        },
+        {
+            "x1": 164,
+            "y1": 660,
+            "x2": 179,
+            "y2": 788,
+            "text": "そして強力な魔剣を",
+            "vi": "Và một con dao ma thuật mạnh mẽ"
+        },
+        {
+            "x1": 122,
+            "y1": 660,
+            "x2": 138,
+            "y2": 760,
+            "text": "目標なんだとか",
+            "vi": "Đó là một mục tiêu"
+        }
+    ],
+    "007.jpg": [
+        {
+            "x1": 253,
+            "y1": 4,
+            "x2": 500,
+            "y2": 127,
+            "text": "魔剣士",
+            "vi": "Kiến kiếm ma thuật"
+        },
+        {
+            "x1": 486,
+            "y1": 120,
+            "x2": 545,
+            "y2": 248,
+            "text": "魔剣を手にスれれば 魔剣士を 名乗れるんだよー！",
+            "vi": "Nếu bạn nhận được một thanh kiếm ma thuật trong tay, bạn có thể yêu cầu một kiếm sĩ ma thuật!"
+        },
+        {
+            "x1": 82,
+            "y1": 119,
+            "x2": 99,
+            "y2": 244,
+            "text": "色々あるんですね",
+            "vi": "Có nhiều thứ khác nhau"
+        },
+        {
+            "x1": 483,
+            "y1": 337,
+            "x2": 550,
+            "y2": 453,
+            "text": "そうだねー あとは聖剣とか 暗黒剣かなー？",
+            "vi": "Đúng vậy -đó là một thanh kiếm thánh hay một thanh kiếm đen?"
+        },
+        {
+            "x1": 85,
+            "y1": 337,
+            "x2": 123,
+            "y2": 464,
+            "text": "ほほう そんなものまでる。",
+            "vi": "Hoho là một điều như vậy."
+        },
+        {
+            "x1": 522,
+            "y1": 536,
+            "x2": 576,
+            "y2": 632,
+            "text": "聖剣",
+            "vi": "Kiếm thánh"
+        },
+        {
+            "x1": 468,
+            "y1": 611,
+            "x2": 506,
+            "y2": 751,
+            "text": "神が授けた神聖な力を 秘めた強力な剣",
+            "vi": "Một thanh kiếm mạnh mẽ với sức mạnh thiêng liêng do Chúa ban cho"
+        },
+        {
+            "x1": 393,
+            "y1": 652,
+            "x2": 450,
+            "y2": 847,
+            "text": "神聖属性が付いていて 物によっては更に強力な 特殊な能力を秘めているらしい",
+            "vi": "Có vẻ như nó có một thuộc tính thiêng liêng và có một khả năng đặc biệt mạnh mẽ hơn tùy thuộc vào sự việc."
+        },
+        {
+            "x1": 59,
+            "y1": 669,
+            "x2": 116,
+            "y2": 877,
+            "text": "全ての属性に対して 優位な属性であり それ故に聖剣であるというたけて",
+            "vi": "Đó là một thuộc tính thống trị cho tất cả các thuộc tính, và nó là một thanh kiếm thiêng liêng."
+        }
+    ],
+    "008.jpg": [
+        {
+            "x1": 576,
+            "y1": 70,
+            "x2": 620,
+            "y2": 186,
+            "text": "暗黒剣",
+            "vi": "Kiếm tối"
+        },
+        {
+            "x1": 437,
+            "y1": 135,
+            "x2": 561,
+            "y2": 312,
+            "text": "これは暗黒神が 授けた邪悪な剣 とか何とか？ 暗黒属性でそれ以外の 基本的な性能で言えば 聖剣と同じ",
+            "vi": "Đây có phải là một thanh kiếm xấu xa được ban cho bởi vị thần bóng tối? Thuộc tính tối giống như Thánh Kiếm trong hiệu suất cơ bản khác"
+        },
+        {
+            "x1": 91,
+            "y1": 107,
+            "x2": 170,
+            "y2": 294,
+            "text": "但し 聖剣と暗黒剣はお互いが 弱点同生であるららしく 打ち合った場合は 格が上の剣が勝つらしい",
+            "vi": "Tuy nhiên, nếu thanh kiếm thiêng liêng và thanh kiếm tối giống nhau nếu chúng có cùng điểm yếu, thì thanh kiếm ở cấp cao hơn sẽ giành chiến thắng."
+        },
+        {
+            "x1": 572,
+            "y1": 380,
+            "x2": 602,
+            "y2": 524,
+            "text": "最後に神剣",
+            "vi": "Cuối cùng là một thanh kiếm thần"
+        },
+        {
+            "x1": 458,
+            "y1": 477,
+            "x2": 532,
+            "y2": 631,
+            "text": "岳説とが言い伝えに ちょっと存在が語られる 程度らしく詳しい事は わかっていない",
+            "vi": "Tôi không biết các chi tiết của Mt."
+        },
+        {
+            "x1": 114,
+            "y1": 473,
+            "x2": 155,
+            "y2": 606,
+            "text": "日く あらゆる物を 切り裂く とか",
+            "vi": "Cây mọi thứ dưới ánh mặt trời"
+        },
+        {
+            "x1": 553,
+            "y1": 705,
+            "x2": 569,
+            "y2": 817,
+            "text": "ところでしょうか",
+            "vi": "Nó là một nơi?"
+        },
+        {
+            "x1": 97,
+            "y1": 756,
+            "x2": 158,
+            "y2": 850,
+            "text": "おー 剣士先生",
+            "vi": "Oh, một giáo viên kiếm sĩ"
+        }
+    ],
+    "009.jpg": [
+        {
+            "x1": 518,
+            "y1": 93,
+            "x2": 553,
+            "y2": 156,
+            "text": "でもまあ なんだね",
+            "vi": "Nhưng nó là cái gì?"
+        },
+        {
+            "x1": 456,
+            "y1": 193,
+            "x2": 512,
+            "y2": 296,
+            "text": "最後の神剣とか 言うのはよく わかららないけど",
+            "vi": "Tôi không chắc chắn nói rằng Thanh kiếm Chúa cuối cùng"
+        },
+        {
+            "x1": 146,
+            "y1": 96,
+            "x2": 225,
+            "y2": 253,
+            "text": "事ならら聖剣と暗黒剣は ぶつちゃけ創造魔法で 何とか出来そうな 気もする訳で",
+            "vi": "Thanh kiếm thiêng liêng và thanh kiếm tối dường như có thể quản lý bằng sự kỳ diệu của sự sáng tạo."
+        },
+        {
+            "x1": 49,
+            "y1": 181,
+            "x2": 125,
+            "y2": 324,
+            "text": "そう考えると アリサさんにはを とてもありがたい話を じてもらった気かする",
+            "vi": "Nếu bạn nghĩ như vậy, tôi cảm thấy Alisa có một câu chuyện rất biết ơn."
+        },
+        {
+            "x1": 122,
+            "y1": 402,
+            "x2": 158,
+            "y2": 515,
+            "text": "そんな簡単に れるもんなの",
+            "vi": "Thật là một điều dễ dàng"
+        },
+        {
+            "x1": 523,
+            "y1": 466,
+            "x2": 547,
+            "y2": 612,
+            "text": "うん 取り敢えず",
+            "vi": "Vâng vào thời điểm hiện tại"
+        },
+        {
+            "x1": 48,
+            "y1": 515,
+            "x2": 82,
+            "y2": 629,
+            "text": "まさかー！ でもレンさんなら",
+            "vi": "Không có cách nào! Nhưng nếu bạn là Len"
+        },
+        {
+            "x1": 456,
+            "y1": 604,
+            "x2": 505,
+            "y2": 809,
+            "text": "まずは魔剣を 目標に頑張ってみようか",
+            "vi": "Trước hết, hãy cố gắng hết sức với mục tiêu của Thanh kiếm ma thuật"
+        },
+        {
+            "x1": 116,
+            "y1": 787,
+            "x2": 144,
+            "y2": 852,
+            "text": "ぬふふ",
+            "vi": "Nufufufu"
+        }
+    ],
+    "011.jpg": [
+        {
+            "x1": 184,
+            "y1": 802,
+            "x2": 450,
+            "y2": 844,
+            "text": "王都アスカ回ン",
+            "vi": "Thủ đô Hoàng gia Asuka mười"
+        }
+    ]
+}
